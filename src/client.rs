3f8e4cbe4d5fbac708a5e4cf1c2dff4e4a9a8e92
zazedd <leomendesantos@gmail.com>
2024-01-23 23:23:06 +0000

    sync, encryption and decryption working, 30-50ms

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,15 @@
 use std::io::{self, Write};
+
+use base64::prelude::*;
 use std::{net::TcpStream, thread::sleep, time::Duration};
 use tungstenite::{connect, stream::MaybeTlsStream, util::NonBlockingError, Message, WebSocket};
 use url::Url;
+
+enum ClientState {
+    Syncing,
+    Encrypting,
+    Encrypted(String),
+}
 
 fn lorenz_attractor(
     x: f64,
@@ -36,6 +44,29 @@ fn receive_msg(socket: &mut WebSocket<MaybeTlsStream<TcpStream>>) -> () {
     println!("Recieved: {}", msg);
 }
 
+fn read_non_blocking(socket: &mut WebSocket<MaybeTlsStream<TcpStream>>) -> Option<Message> {
+    match socket.read() {
+        Ok(msg) => Some(msg),
+        Err(err) => match err.into_non_blocking() {
+            Some(e) => panic!("Panic at message {}", e),
+            None => None,
+        },
+    }
+}
+
+fn encrypt(message: &str, key_stream: &[u8]) -> Vec<u8> {
+    let message_bytes = message.as_bytes();
+    let mut ciphertext = Vec::new();
+
+    for (i, &byte) in message_bytes.iter().enumerate() {
+        let key_byte = key_stream[i % key_stream.len()];
+        let encrypted_byte = byte ^ key_byte;
+        ciphertext.push(encrypted_byte);
+    }
+
+    ciphertext
+}
+
 fn main() {
     env_logger::init();
 
@@ -54,10 +85,10 @@ fn main() {
     let rho = 28.0;
     let beta = 8.0 / 3.0;
     let h = 0.01;
-    let mut syncing = false;
+    let mut stream_state = ClientState::Syncing;
+    let mut key_stream = Vec::new();
 
     let mut state = lorenz_attractor(seed.0, seed.1, seed.2, sigma, rho, beta, h);
-    println!("x = {}, y = {}, z = {}", state.0, state.1, state.2);
 
     let mut input = String::new();
     print!("Type a message you want to encrypt: ");
@@ -68,22 +99,73 @@ fn main() {
     send_request(&mut socket, "Sync Request", 1);
     receive_msg(&mut socket);
 
-    loop {
-        state = lorenz_attractor(state.0, state.1, state.2, sigma, rho, beta, h);
-        socket
-            .send(Message::Binary(state.0.to_ne_bytes().to_vec()))
-            .expect("oopsie");
-        socket
-            .send(Message::Binary(state.1.to_ne_bytes().to_vec()))
-            .expect("oopsie");
-        socket
-            .send(Message::Binary(state.2.to_ne_bytes().to_vec()))
-            .expect("oopsie");
-
-        println!("x = {}, y = {}, z = {}", state.0, state.1, state.2);
-
-        // Allows for a new message to be created, making the sync better, but
-        // slower
-        sleep(Duration::new(0, 5000000));
+    match socket.get_mut() {
+        tungstenite::stream::MaybeTlsStream::Plain(stream) => stream.set_nonblocking(true),
+        _ => unimplemented!(),
     }
+    .expect("Could not make socket non-blocking");
+
+    loop {
+        match stream_state {
+            ClientState::Syncing => {
+                state = lorenz_attractor(state.0, state.1, state.2, sigma, rho, beta, h);
+                socket
+                    .send(Message::Binary(state.0.to_ne_bytes().to_vec()))
+                    .expect("Could not send x coordinate");
+                socket
+                    .send(Message::Binary(state.1.to_ne_bytes().to_vec()))
+                    .expect("Could not send y coordinate");
+                socket
+                    .send(Message::Binary(state.2.to_ne_bytes().to_vec()))
+                    .expect("Could not send z coordinate");
+
+                println!("state = {:?}", state);
+
+                match read_non_blocking(&mut socket) {
+                    Some(Message::Binary(v)) if v.as_slice() == [2] => {
+                        println!("Server finished syncing. Encrypting now");
+                        stream_state = ClientState::Encrypting;
+                    }
+                    _ => (),
+                }
+
+                // Allows for a new message to be created, making the sync better, but
+                // slower
+                // sleep(Duration::new(0, 5000));
+            }
+
+            ClientState::Encrypting => {
+                if key_stream.len() == 16 {
+                    let ciphertext = BASE64_STANDARD.encode(encrypt(input.as_str(), &key_stream));
+                    stream_state = ClientState::Encrypted(ciphertext);
+                    continue;
+                }
+
+                state = lorenz_attractor(state.0, state.1, state.2, sigma, rho, beta, h);
+
+                let bytes = state.1.to_ne_bytes();
+                bytes.iter().for_each(|e| key_stream.push(*e));
+            }
+
+            ClientState::Encrypted(ref ciphertext) => {
+                println!("Finished encrypting with message = {}", ciphertext);
+                println!("Sending encrypted message");
+
+                send_request(&mut socket, "Encryption Completed", 3);
+
+                socket
+                    .send(Message::Text(ciphertext.to_string()))
+                    .expect("Could not send ciphertext");
+
+                let fst: u8 = key_stream.first().expect("No first").to_owned();
+                socket
+                    .send(Message::Binary(vec![fst]))
+                    .expect("Could not send fst");
+
+                break;
+            }
+        }
+    }
+
+    println!("Done. Bye bye");
 }
--- src/server.rs
+++ src/server.rs
@@ -2,7 +2,7 @@ use std::{
     net::TcpListener,
     net::TcpStream,
     thread::{sleep, spawn},
-    time::Duration,
+    time::{Duration, SystemTime},
 };
 
 use tungstenite::{
@@ -12,6 +12,16 @@ use tungstenite::{
     util::NonBlockingError,
     Message, WebSocket,
 };
+
+use base64::prelude::*;
+
+enum ServerState {
+    Unsynced,
+    Syncing,
+    Synced,
+    Encrypted,
+    Decrypted(String),
+}
 
 fn lorenz_attractor(
     x: f64,
@@ -41,6 +51,14 @@ fn lorenz_attractor(
     }
 }
 
+fn send_request(socket: &mut WebSocket<TcpStream>, name: &str, request_id: u8) -> () {
+    socket
+        .send(Message::Binary(vec![request_id]))
+        .expect(format!("Unable to send request: {}", name).as_str());
+
+    println!("Sent: {}", name);
+}
+
 fn read_non_blocking(socket: &mut WebSocket<TcpStream>) -> Option<Message> {
     match socket.read() {
         Ok(msg) => Some(msg),
@@ -49,6 +67,19 @@ fn read_non_blocking(socket: &mut WebSocket<TcpStream>) -> Option<Message> {
             None => None,
         },
     }
+}
+
+fn dencrypt(base64_message: &str, key_stream: &[u8]) -> Vec<u8> {
+    let encrypted_message_bytes = BASE64_STANDARD.decode(base64_message).unwrap();
+    let mut decrypted_message = Vec::new();
+
+    for (i, &byte) in encrypted_message_bytes.iter().enumerate() {
+        let key_byte = key_stream[i % key_stream.len()];
+        let encrypted_byte = byte ^ key_byte;
+        decrypted_message.push(encrypted_byte);
+    }
+
+    decrypted_message
 }
 
 fn main() {
@@ -76,6 +107,8 @@ fn main() {
             };
             let mut websocket = accept_hdr(stream.unwrap(), callback).unwrap();
 
+            let mut stream_state = ServerState::Unsynced;
+
             websocket
                 .get_mut()
                 .set_nonblocking(true)
@@ -86,75 +119,135 @@ fn main() {
             let rho = 28.0;
             let beta = 8.0 / 3.0;
             let h = 0.01;
-            let mut syncing = false;
             let mut last_y = 0.;
             let mut last_z = 0.;
             let mut sync_count = 0;
+            let mut key_stream = Vec::new();
+            let mut time = SystemTime::now();
 
             loop {
-                if !syncing {
-                    let (new_x, new_y, new_z) =
-                        lorenz_attractor(seed.0, None, seed.1, seed.2, sigma, rho, beta, h);
-                    seed = (new_x, new_y, new_z);
-
-                    println!("coords = {:?}", seed);
-
-                    if let Some(Message::Binary(v)) = read_non_blocking(&mut websocket) {
-                        if v.as_slice() == [1] {
-                            println!("Received: Sync Request");
-                            websocket
-                                .send(Message::Text("Sync Request approved".to_string()))
-                                .unwrap();
-                            syncing = true
-                        }
-                    }
-
-                    sleep(Duration::new(0, 5000000));
-                    continue;
-                }
-                websocket
-                    .get_mut()
-                    .set_nonblocking(false)
-                    .expect("Couldn't make socket blocking");
-
-                match (websocket.read(), websocket.read(), websocket.read()) {
-                    (
-                        Ok(Message::Binary(x_prime_msg)),
-                        Ok(Message::Binary(y_prime_msg)),
-                        Ok(Message::Binary(z_prime_msg)),
-                    ) => {
-                        let x_prime = f64::from_ne_bytes(x_prime_msg[0..8].try_into().unwrap());
-                        let y_prime = f64::from_ne_bytes(y_prime_msg[0..8].try_into().unwrap());
-                        let z_prime = f64::from_ne_bytes(z_prime_msg[0..8].try_into().unwrap());
-                        let (new_x, new_y, new_z) = lorenz_attractor(
-                            seed.0,
-                            Some(x_prime),
-                            seed.1,
-                            seed.2,
-                            sigma,
-                            rho,
-                            beta,
-                            h,
-                        );
+                match stream_state {
+                    ServerState::Unsynced => {
+                        let (new_x, new_y, new_z) =
+                            lorenz_attractor(seed.0, None, seed.1, seed.2, sigma, rho, beta, h);
                         seed = (new_x, new_y, new_z);
 
-                        println!("last_y = {}, y_prime = {}", last_y, y_prime);
-                        if last_y == y_prime && last_z == z_prime {
-                            sync_count += 1;
-                            println!("syncd!!");
-                            if sync_count == 10 {
-                                syncing = false;
+                        // println!("seed = {:?}", seed);
+
+                        if let Some(Message::Binary(v)) = read_non_blocking(&mut websocket) {
+                            if v.as_slice() == [1] {
+                                time = SystemTime::now();
+                                println!("Received: Sync Request");
+                                websocket
+                                    .send(Message::Text("Sync Request approved".to_string()))
+                                    .unwrap();
+                                stream_state = ServerState::Syncing;
                             }
-                        } else {
-                            sync_count = 0;
                         }
-                        // Allows for a new message to be created, making the sync better, but
-                        // slower
-                        sleep(Duration::new(0, 5000000));
-                        last_y = new_y;
-                        last_z = new_z;
+
+                        // sleep(Duration::new(0, 5000));
                     }
-                    _ => panic!("oops"),
+                    ServerState::Syncing => {
+                        websocket
+                            .get_mut()
+                            .set_nonblocking(false)
+                            .expect("Couldn't make socket blocking");
+
+                        match (websocket.read(), websocket.read(), websocket.read()) {
+                            (
+                                Ok(Message::Binary(x_prime_msg)),
+                                Ok(Message::Binary(y_prime_msg)),
+                                Ok(Message::Binary(z_prime_msg)),
+                            ) => {
+                                let x_prime =
+                                    f64::from_ne_bytes(x_prime_msg[0..8].try_into().unwrap());
+                                let y_prime =
+                                    f64::from_ne_bytes(y_prime_msg[0..8].try_into().unwrap());
+                                let z_prime =
+                                    f64::from_ne_bytes(z_prime_msg[0..8].try_into().unwrap());
+                                let (new_x, new_y, new_z) = lorenz_attractor(
+                                    seed.0,
+                                    Some(x_prime),
+                                    seed.1,
+                                    seed.2,
+                                    sigma,
+                                    rho,
+                                    beta,
+                                    h,
+                                );
+                                seed = (new_x, new_y, new_z);
+
+                                // println!("seed = {:?}", seed);
+
+                                if last_y == y_prime && last_z == z_prime {
+                                    sync_count += 1;
+                                    println!("Lorenz Attractors are synced");
+                                    if sync_count == 100 {
+                                        send_request(&mut websocket, "Sync Complete", 2);
+                                        stream_state = ServerState::Synced;
+                                    }
+                                } else {
+                                    sync_count = 0;
+                                }
+                                // Allows for a new message to be created, making the sync better, but
+                                // slower
+                                // sleep(Duration::new(0, 5000));
+                                last_y = new_y;
+                                last_z = new_z;
+                            }
+                            _ => panic!("Received invalid data format"),
+                        }
+                    }
+                    ServerState::Synced => {
+                        websocket
+                            .get_mut()
+                            .set_nonblocking(true)
+                            .expect("Couldn't make socket non-blocking");
+
+                        let (new_x, new_y, new_z) =
+                            lorenz_attractor(seed.0, None, seed.1, seed.2, sigma, rho, beta, h);
+                        seed = (new_x, new_y, new_z);
+
+                        let bytes = seed.1.to_ne_bytes();
+                        bytes.iter().for_each(|e| key_stream.push(*e));
+
+                        match read_non_blocking(&mut websocket) {
+                            Some(Message::Binary(v)) if v.as_slice() == [3] => {
+                                stream_state = ServerState::Encrypted
+                            }
+                            _ => (),
+                        }
+                    }
+                    ServerState::Encrypted => {
+                        websocket
+                            .get_mut()
+                            .set_nonblocking(false)
+                            .expect("Couldn't make socket blocking");
+                        match (websocket.read(), websocket.read()) {
+                            (Ok(Message::Text(ciphertext)), Ok(Message::Binary(fst))) => {
+                                println!("Received ciphertext = {}", ciphertext);
+
+                                let first_item = fst.first().unwrap().to_owned();
+                                match key_stream.iter().position(|&item| item == first_item) {
+                                    Some(index) => {
+                                        let new_key_stream = key_stream[index..index + 16].to_vec();
+                                        let decoded_message =
+                                            dencrypt(&ciphertext, &new_key_stream);
+                                        stream_state = ServerState::Decrypted(
+                                            String::from_utf8(decoded_message).unwrap(),
+                                        );
+                                    }
+                                    None => panic!("Item {} not found in the vector.", first_item),
+                                }
+                            }
+                            _ => panic!("Invalid message received"),
+                        }
+                    }
+                    ServerState::Decrypted(ref plaintext) => {
+                        println!("Decoded message: {}", plaintext);
+                        println!("Took: {}ms", time.elapsed().unwrap().as_millis());
+                        break;
+                    }
                 }
             }
         });
